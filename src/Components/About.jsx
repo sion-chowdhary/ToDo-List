7a57bab1cecb447fc67e735ee7a37e62a2a49b80
SION CHOWDHARY <94757639+sion-chowdhary@users.noreply.github.com>
2023-02-09 17:00:45 +0530

    first commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='my-3 mx-2'>
+        This is an About Component
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus tenetur nesciunt molestias error. Voluptates, doloremque inventore nesciunt necessitatibus, eligendi praesentium velit quasi, ea fugiat sint ipsam vero quis! Nesciunt distinctio deleniti voluptatum corrupti illum cupiditate quo facere ullam illo earum enim aut optio, quia in autem dolore a voluptatibus blanditiis! Magni quae provident eius!</p>
+    </div>
+  )
+}
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerStyle = {
+  //   position:"relative",
+  //   top:"10vh",
+  //   width:"100%"
+  // }
+  // style={footerStyle}
+  return (
+     <footer className="bg-dark text-light py-3" >
+        <p className='text-center'>
+         CopyRight &copy; MyTodosList.com
+        </p>
+    </footer>
+  )
+}
